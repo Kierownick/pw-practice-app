992e1db79c523081fe3e7d34991ed34edd855b37
Adrian Karwowski <53815823+Kierownick@users.noreply.github.com>
2026-01-29 22:24:22 +0100

    PW2

--- tests/firstTest.spec.ts
+++ tests/firstTest.spec.ts
@@ -5,18 +5,44 @@ import { test } from '@playwright/test';
 test.beforeEach(async ({ page }) => {
     await page.goto('http://localhost:4200');
     await page.getByText('Forms').click();
+    await page.getByText('Form Layouts').click();
 });
 
-test.describe.skip('First Test Suite', () => {
-    test.beforeEach(async ({ page }) => {
-        await page.getByText('Charts').first().click();
-    });
+test('Locator Syntax Rules', async ({ page }) => {
+//by Tag Name
+    await page.locator('input').first().click();
 
-    test('first test', async ({ page }) => {
-        await page.getByText('Form Layouts').click();
-    });
+//by ID
+    await page.locator('#inputEmail1').first().click();
 
-    test ('second test', async ({ page }) => {
-        await page.getByText('Datepicker').click();
-    })
-})
+//by Class Value
+    await page.locator('.shape-rectangle').click();
+
+//by Attribute Name
+    page.locator('[placeholder="Email"]');
+
+//by Class value (full)
+    page.locator('[class="input-full-width size-medium status-basic shape-rectangle nb-transition"]')
+
+//by Combination
+    page.locator('input[placeholder="Email"]');
+
+//by Xpath
+    page.locator('//*[@id="inputEmail1"]');
+
+//by Partial Text
+    page.locator(':text("Using")');
+
+//by Exact Text Match
+    page.locator('text="Using the Grid"');
+
+
+});
+
+test.only('User facing locators', async ({ page }) => {
+    await page.getByRole('textbox', { name: 'Email' }).first().click();
+    await page.getByRole('button', { name: 'Sign in' }).first().click();
+    await page.getByLabel('Email').first().click();
+    await page.getByPlaceholder('Jane Doe').click();
+
+});
